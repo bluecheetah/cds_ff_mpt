c71de788586d1f351d54c4f21bcb362fc271cf88
Nathan Narevsky <nathan@bcanalog.com>
2020-03-02 15:56:15 -0800

    Adding files for initial release

---
+++ src/BAG_prim/schematic/pmos4_standard.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any
+
+
+from bag.design.module import MosModuleBase
+from bag.design.database import ModuleDB
+from bag.util.immutable import Param
+
+
+# noinspection PyPep8Naming
+class BAG_prim__pmos4_standard(MosModuleBase):
+    """design module for BAG_prim__pmos4_standard.
+    """
+
+    def __init__(self, database: ModuleDB, params: Param, **kwargs: Any) -> None:
+        MosModuleBase.__init__(self, '', database, params, **kwargs)
+
---
+++ src/BAG_prim/schematic/res_metal_7.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any
+
+
+from bag.design.module import ResMetalModule
+from bag.design.database import ModuleDB
+from bag.util.immutable import Param
+
+
+# noinspection PyPep8Naming
+class BAG_prim__res_metal_7(ResMetalModule):
+    """design module for BAG_prim__res_metal_7.
+    """
+
+    def __init__(self, database: ModuleDB, params: Param, **kwargs: Any) -> None:
+        ResMetalModule.__init__(self, '', database, params, **kwargs)
+
